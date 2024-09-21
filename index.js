c54662eea3f048283ada252ef1ac77a746174773
swet-s <shreet.swet@gmail.com>
2024-09-22 00:51:52 +0530

    Add cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
 const connectDB = require("./db");
 connectDB();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // Available Routes
